3051088c3b16c67f88e83083627f05793260470e
Mike <53232564+michern1102@users.noreply.github.com>
2023-08-24 14:12:37 -0600

    Update Rush-Migration.ps1

--- Rush-Migration.ps1
+++ Rush-Migration.ps1
@@ -1,14 +1,28 @@
 #Root Folder - Anything running off this script will come from this folder
-$RootFolder = "C:\Scripts\Rush\"
+$RootFolder = "C:\Scripts"
 
 #Connecting to all vCenters
-$vcenterlist = get-content "$RootFolder\vCenters\vcentersPROD.txt"
+$Sourcevcenterlist = get-content "$RootFolder\vCenters\vcenterSOURCE.txt"
+
+$Targetvcenterlist = get-content "$RootFolder\vCenters\vcenterTARGET.txt"
+
 
 #make sure to have a credential file already created and ready in the proper folder
-foreach ($myvcenter in $vcenterlist) {
-    Write-Host Connecting to $myvcenter
-    $MyCredentials = Get-Credential
-    Try {connect-viserver $myvcenter -Credential $MyCredentials} Catch {Connection-Alert $myvcenter ;break}
+
+Write-Host "Please add your SOURCE (Old) vCenter Credentials"
+$sourceVCSACredentials = Get-Credential
+
+Write-Host "Please add your TARGET (New) vCenter Credentials"
+$targetVCSACredentials = Get-Credential
+
+foreach ($Sourcevcenter in $Sourcevcenterlist) {
+    Write-Host "Connecting to $Sourcevcenter"
+    Try {connect-viserver $Sourcevcenter -Credential $sourceVCSACredentials} Catch {Connection-Alert $Sourcevcenter ;break}
+    }
+
+foreach ($Targetvcenter in $Targetvcenterlist) {
+    Write-Host Connecting to "$Targetvcenter"
+    Try {connect-viserver $Targetvcenter -Credential $targetVCSACredentials} Catch {Connection-Alert $Targetvcenter ;break}
     }
 
 #Get all VM an Host Variables
@@ -23,41 +37,42 @@ $targetDPG = ""
 
 ##########################################################################################################################
 
-#Source Cluster Selection Box
+#Source Host Selection Box
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 $form = New-Object System.Windows.Forms.Form
 $form.Text = 'Select a Source Host for VM Migration'
-$form.Size = New-Object System.Drawing.Size(300,200)
+$form.Size = New-Object System.Drawing.Size(600,400)
 $form.StartPosition = 'CenterScreen'
 
 $okButton = New-Object System.Windows.Forms.Button
-$okButton.Location = New-Object System.Drawing.Point(75,120)
-$okButton.Size = New-Object System.Drawing.Size(75,23)
+$okButton.Location = New-Object System.Drawing.Point(150,240)
+$okButton.Size = New-Object System.Drawing.Size(150,46)
 $okButton.Text = 'OK'
 $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
 $form.AcceptButton = $okButton
 $form.Controls.Add($okButton)
 
 $cancelButton = New-Object System.Windows.Forms.Button
-$cancelButton.Location = New-Object System.Drawing.Point(150,120)
-$cancelButton.Size = New-Object System.Drawing.Size(75,23)
+$cancelButton.Location = New-Object System.Drawing.Point(300,240)
+$cancelButton.Size = New-Object System.Drawing.Size(150,46)
 $cancelButton.Text = 'Cancel'
 $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
 $form.CancelButton = $cancelButton
 $form.Controls.Add($cancelButton)
 
 $label = New-Object System.Windows.Forms.Label
-$label.Location = New-Object System.Drawing.Point(10,20)
-$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Location = New-Object System.Drawing.Point(20,40)
+$label.Size = New-Object System.Drawing.Size(560,40)
 $label.Text = 'Select a Source Host for VM Migration:'
 $form.Controls.Add($label)
 
 $listBox = New-Object System.Windows.Forms.ListBox
-$listBox.Location = New-Object System.Drawing.Point(10,40)
-$listBox.Size = New-Object System.Drawing.Size(260,20)
-$listBox.Height = 80
+$listBox.Location = New-Object System.Drawing.Point(20,80)
+$listBox.Size = New-Object System.Drawing.Size(520,40)
+$listBox.Height = 160
+$listBox.Font = New-Object System.Drawing.Font("Lucida Console",12,[System.Drawing.FontStyle]::Regular)
 $listbox.SelectionMode = 'MultiExtended'
 
 foreach($singleHost in $GetallHosts){
@@ -79,7 +94,7 @@ if ($result -eq [System.Windows.Forms.DialogResult]::OK){
 ############################################################################################################################
 
 
-#Source Cluster Selection Box
+#Target Cluster Selection Box
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
@@ -136,7 +151,7 @@ if ($result -eq [System.Windows.Forms.DialogResult]::OK){
 
 #Collect all VM's from source Host
 Write-Host "Collecting VM's on host"
-$HostVMs = Get-Host -Name $SourceHost | Get-VM | where {($_.ExtensionData.Config.ManagedBy.ExtensionKey -notlike 'com.vmware.vcDr*')}
+$HostVMs = Get-VMHost -Name $SourceHost | Get-VM | where {($_.ExtensionData.Config.ManagedBy.ExtensionKey -notlike '*com.vmware.vcDr*')}
 Start-sleep 3
 
 #exclusion portion
@@ -152,6 +167,7 @@ foreach ($singlevm in $HostVMs){
     }
 }
 
+
 Start-Sleep 3
 
 #Execution portion
@@ -162,11 +178,14 @@ foreach ($singlevm in $HostVMs){
     else{
         #find the folder the VM is currently using and verify folder exists on destination VCSA
         $TargetVMFolderCheck = get-datacenter -name "TARGET DATACENTER" | Get-Folder | where {$_.type -eq "VM"}
-         if ($TargetVMFolderCheck -notcontains $Singlevm.Folder.Name){
+         if ($TargetVMFolderCheck.Name -notcontains $Singlevm.Folder.Name){
             Write-host "WARNING - Folder not discovered in target Cluster! Migration will Fail."
+            Write-host "------------------------------------------------------------------------"
+            Read-Host "Press any key to continue"
          }
 
         #Start pings
+        Write-Host "Creating continuous ping for VM $singlevm"
         $FirstIP = (get-vm $singlevm).Guest.IPAddress[0] 
         $args = "/k ping $FirstIP -t"
         Start-Process -FilePath "$env:comspec" -ArgumentList $args
@@ -176,10 +195,10 @@ foreach ($singlevm in $HostVMs){
 
 
         #Select random host from Target Cluster to migrate to
-        $targetHost = get-Cluster $TargetCluster -Server $DestVC | get-vmhost | where{$_.ConnectionState -eq “Connected”}| get-random
+        $targetHost = get-Cluster $TargetCluster | get-vmhost | where{$_.ConnectionState -eq “Connected”}| get-random
 
         #Get Target VM Folder name
-        $TargetVMFolder = Get-Datacenter -name "TARGET-DATACENTER" | Get-Folder | where{($_.type -eq "VM") -and ($_.name -eq $singlevm.Folder.Name)}
+        $TargetVMFolder = Get-Datacenter -name "RUMC" | Get-Folder | where{($_.type -eq "VM") -and ($_.name -eq $singlevm.Folder.Name)}
 
 
         #start VM Migration
@@ -188,17 +207,12 @@ foreach ($singlevm in $HostVMs){
 
         #start Folder Migration
         Write-Host "Migrating vm $singlevm to Target VCSA Folder $TargetVMFolder"
+        $singlevm | Move-VM -Destination $TargetVMFolder -WhatIf
 
-        #Re-Apply Tags
-        $vmtags | %{get-vm $singlevm -Server $DestVC | New-TagAssignment -Tag ($_.Tag).name -Server $DestVC | Select Entity, Tag}
+        #Re-Apply Tags (needs work)
+        #$vmtags | %{get-vm $singlevm -Server $DestVC | New-TagAssignment -Tag ($_.Tag).name -Server $DestVC | Select Entity, Tag}
 
 
         
     }
 }
-
-
-
-
-
-
